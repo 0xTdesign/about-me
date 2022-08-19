2ded42f3d31628ad3b4c332034804d18d3a03027
Wollivan <tim.smith2814@gmail.com>
2022-08-19 09:23:57 +0100

    Changes made together

--- app.js
+++ app.js
@@ -19,71 +19,71 @@ function startQuiz() {
     );
   }
 
-  // if (question === "yes") {
-  //   alert("Great," + " " + name + " " + "Lets get started");
-  //   // console.log("Great lets get started");
-  // } else {
-  //   // console.log("You sure you dont wanna try");
-  //   alert(name + "You sure you dont wanna try");
-  // }
+  if (question === "yes") {
+    alert("Great," + " " + name + " " + "Lets get started");
+    // console.log("Great lets get started");
+  } else {
+    // console.log("You sure you dont wanna try");
+    alert(name + "You sure you dont wanna try");
+  }
 
-  // let dubai = prompt("Have you ever been to Dubai? Yes or No").toLowerCase();
-  // while (dubai !== "yes" && dubai !== "no") {
-  //   // console.log("Please only anwser yes or no");
-  //   alert("Please only anwser yes or no");
-  //   dubai = prompt("Have you ever been to Dubai? Yes or No").toLowerCase();
-  // }
-  // if (dubai === "yes") {
-  //   // console.log(dubai);
-  //   alert("Amazing hope you liked it");
-  // } else {
-  //   console.log("Maybe some other time");
-  //   alert("Maybe you can go next holiday");
-  // }
+  let dubai = prompt("Have you ever been to Dubai? Yes or No").toLowerCase();
+  while (dubai !== "yes" && dubai !== "no") {
+    // console.log("Please only anwser yes or no");
+    alert("Please only anwser yes or no");
+    dubai = prompt("Have you ever been to Dubai? Yes or No").toLowerCase();
+  }
+  if (dubai === "yes") {
+    // console.log(dubai);
+    alert("Amazing hope you liked it");
+  } else {
+    console.log("Maybe some other time");
+    alert("Maybe you can go next holiday");
+  }
 
-  // let cat = prompt("Do you have a cat?").toLowerCase();
-  // while (cat !== "yes" && cat !== "no") {
-  //   // console.log("Please only anwser yes or no");
-  //   alert("Please only anwser yes or no");
-  //   dubai = prompt("Do you have a cat?").toLowerCase();
-  // }
-  // if (cat === "yes") {
-  //   // console.log(cat);
-  //   alert(name + " I love cats as well");
-  // } else {
-  //   // console.log("Maybe some other time");
-  //   alert("Not everyone likes cats");
-  // }
+  let cat = prompt("Do you have a cat?").toLowerCase();
+  while (cat !== "yes" && cat !== "no") {
+    // console.log("Please only anwser yes or no");
+    alert("Please only anwser yes or no");
+    dubai = prompt("Do you have a cat?").toLowerCase();
+  }
+  if (cat === "yes") {
+    // console.log(cat);
+    alert(name + " I love cats as well");
+  } else {
+    // console.log("Maybe some other time");
+    alert("Not everyone likes cats");
+  }
 
-  // let course = prompt(
-  //   "Are you interested in Software Developemnt"
-  // ).toLowerCase();
-  // while (course !== "yes" && course !== "no") {
-  //   // console.log("Please only anwser yes or no");
-  //   alert("Please only anwser yes or no");
-  //   course = prompt("Are you interested in Software Developemnt").toLowerCase();
-  // }
-  // if (course === "yes") {
-  //   // console.log(course);
-  //   alert(name + " That's amazing. There are so many videos and course online");
-  // } else {
-  //   // console.log("Its not for everyone");
-  //   alert("Its not for everyone");
-  // }
+  let course = prompt(
+    "Are you interested in Software Developemnt"
+  ).toLowerCase();
+  while (course !== "yes" && course !== "no") {
+    // console.log("Please only anwser yes or no");
+    alert("Please only anwser yes or no");
+    course = prompt("Are you interested in Software Developemnt").toLowerCase();
+  }
+  if (course === "yes") {
+    // console.log(course);
+    alert(name + " That's amazing. There are so many videos and course online");
+  } else {
+    // console.log("Its not for everyone");
+    alert("Its not for everyone");
+  }
 
-  // let crypto = prompt("Have you ever bought any Crypto?").toLowerCase();
-  // while (crypto !== "yes" && crypto !== "no") {
-  //   // console.log("Please only anwser yes or no");
-  //   alert = "Please only anwser yes or no";
-  //   crypto = prompt("Have you ever bought any Crypto?").toLowerCase();
-
-  //   if (crypto === "yes") {
-  //     // console.log(crypto);
-  //     alert(name + "That's great hope it moons hard");
-  //   } else {
-  //     // console.log("Its not for everyone");
-  //     alert(name + "Its not for everyone");
-  //   }
+  let crypto = prompt("Have you ever bought any Crypto?").toLowerCase();
+  while (crypto !== "yes" && crypto !== "no") {
+    // console.log("Please only anwser yes or no");
+    alert("Please only anwser yes or no");
+    crypto = prompt("Have you ever bought any Crypto?").toLowerCase();
+  }
+  if (crypto === "yes") {
+    // console.log(crypto);
+    alert(name + "That's great hope it moons hard");
+  } else {
+    // console.log("Its not for everyone");
+    alert(name + "Its not for everyone");
+  }
   let game = prompt(
     "Lets play a number guessing game. Pick a number between 1-10"
   ).toLowerCase();
