f2c75e5924a823d524d56eea0fd49e0efd6219a6
jimbobdxb <tallisondxb@gmail.com>
2022-08-19 10:45:10 +0400

    added price tracker to tokens

--- price.js
+++ price.js
@@ -2,7 +2,7 @@
 let tokens = {
   async: true,
   scroosDomain: true,
-  url: "https://api.coingecko.com/api/v3/simple/price?ids=ethereum%2Cbinance-bitcoin%2Cavalanche-2%2Csolana%2Cchikn-egg%2Cbinance-coin-wormhole&vs_currencies=usd",
+  url: "https://api.coingecko.com/api/v3/simple/price?ids=binance-bitcoin%2Cavalanche-2%2Cethereum%2Csolana%2Cchikn-egg%2Cbinance-coin-wormhole%2Cshiba-inu-wormhole%2Cbinance-peg-cardano%2Cbinance-peg-dogecoin%2Cbinance-peg-xrp&vs_currencies=usd",
 
   method: "GET",
   headers: {},
@@ -16,5 +16,9 @@ $.ajax(tokens).done(function (response) {
   eth.innerHTML = response["ethereum"].usd;
   sol.innerHTML = response["solana"].usd;
   egg.innerHTML = response["chikn-egg"].usd;
+  ada.innerHTML = response["binance-peg-cardano"].usd;
+  xrp.innerHTML = response["binance-peg-xrp"].usd;
+  shib.innerHTML = response["shiba-inu-wormhole"].usd;
+  doge.innerHTML = response["binance-peg-dogecoin"].usd;
   bnb.innerHTML = response["binance-coin-wormhole"].usd;
 });
